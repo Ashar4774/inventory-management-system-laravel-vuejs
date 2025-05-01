241e408e33903dd16c5370fa006c318e0752ce71
Ashar4774 <ashar.muhammad74@gmail.com>
2025-05-01 19:20:48 +0500

    added validation

--- resources/js/pages/inventory/Index.vue
+++ resources/js/pages/inventory/Index.vue
@@ -139,17 +139,24 @@ const createInventoryForm = () => {
                                         : 'text-gray-900 border-gray-300 dark:text-white dark:border-gray-600 focus:border-blue-600'
                                 ]" />
                                 <label for="floating_product_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Product Name</label>
-                                <p v-if="form.errors.title" class="mt-1 text-sm text-red-600 dark:text-red-500">
+                                <p v-if="form.errors.title && form.title == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">
                                     {{ form.errors.title }}
                                 </p>
                             </div>
                             <div class="grid md:grid-cols-2 md:gap-6">
                                 <div class="relative z-0 w-full mb-5 group">
-                                    <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="form.category_id">
+                                    <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="form.category_id" :class="[
+                                    form.errors.category_id
+                                        ? 'border-red-500 text-red-900 dark:border-red-400 dark:text-red-400 focus:border-red-600'
+                                        : 'text-gray-900 border-gray-300 dark:text-white dark:border-gray-600 focus:border-blue-600'
+                                ]">
 
                                         <option selected>Category</option>
                                         <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.title }}</option>
                                     </select>
+                                    <p v-if="form.errors.category_id && form.category_id == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">
+                                        {{ form.errors.category_id }}
+                                    </p>
                                 </div>
                                 <div class="relative z-0 w-full mb-5 group">
                                     <input type="number" v-model="form.qty" id="floating_quantity" :class="[
@@ -159,7 +166,7 @@ const createInventoryForm = () => {
                                         : 'text-gray-900 border-gray-300 dark:text-white dark:border-gray-600 focus:border-blue-600'
                                 ]" placeholder=" " value="1" />
                                     <label for="floating_quantity" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Quantity</label>
-                                    <p v-if="form.errors.qty" class="mt-1 text-sm text-red-600 dark:text-red-500">
+                                    <p v-if="form.errors.qty && form.qty == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">
                                         {{ form.errors.qty }}
                                     </p>
                                 </div>
