e498e15d24c8f80451942946710e7311849c6547
Ashar4774 <ashar.muhammad74@gmail.com>
2025-05-12 20:27:51 +0500

    added Vendor list and create components of Vendor; partially

--- resources/js/pages/vendors/Index.vue
+++ resources/js/pages/vendors/Index.vue
@@ -2,11 +2,14 @@
 
 import AppLayout from "@/layouts/AppLayout.vue";
 import {type BreadcrumbItem } from "@/types";
-import {Head} from "@inertiajs/vue3";
+import {Head, useForm} from "@inertiajs/vue3";
 import {PlusIcon, TrashIcon, PenIcon, EyeIcon} from "lucide-vue-next";
+import VendorList from "@/pages/vendors/includes/VendorList.vue";
+import CreateModel from "@/pages/vendors/includes/CreateModel.vue";
+import {ref} from "vue";
 
 const props = defineProps(['vendors']);
-// import VendorList from "@/pages/vendors/includes/VendorList.vue";
+
 // import CreateModel from "@/pages/vendors/includes/CreateModel.vue";
 // import EditModel from "@/pages/vendors/includes/EditModel.vue";
 // import DeleteModel from "@/pages/vendors/includes/DeleteModel.vue";
@@ -17,6 +20,16 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: '/vendor'
     }
 ]
+
+const form = useForm({
+    'id': '',
+    'name': '',
+    'address': '',
+    'phone_no': '',
+    'description': '',
+})
+
+const show_create_vendor_modal = ref('false');
 </script>
 
 <template>
@@ -50,29 +63,118 @@ const breadcrumbs: BreadcrumbItem[] = [
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="vendor in props.vendors" :key="vendor.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{ vendor.name }}
-                            </th>
-                            <td class="px-6 py-4">
-                                {{ vendor.phone_no }}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ vendor.address }}
-                            </td>
-                            <td class="px-6 py-4 flex gap-1">
-                                <button class="text-red-600"><TrashIcon/></button>
-                                <button class="text-green-600"><PenIcon /></button>
-                                <a href="" class="text-blue-600"><EyeIcon /></a>
-                            </td>
-                        </tr>
+                        <VendorList :vendors="vendors" />
                     </tbody>
                 </table>
             </div>
         </div>
 
         <!-- Create Inventory modal -->
+<!--        <div v-if="show_create_vendor_modal" id="create-inventory-modal" class="fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-gray-500">-->
+<!--            <div class="relative p-4 w-full max-w-2xl max-h-full mx-auto">-->
+<!--                &lt;!&ndash; Modal content &ndash;&gt;-->
+<!--                <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">-->
+<!--                    &lt;!&ndash; Modal header &ndash;&gt;-->
+<!--                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">-->
+<!--                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">-->
+<!--                            Create Inventory-->
+<!--                        </h3>-->
+<!--                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" @click="closeVendorCreateModel">-->
+<!--                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">-->
+<!--                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>-->
+<!--                            </svg>-->
+<!--                            <span class="sr-only" >Close modal</span>-->
+<!--                        </button>-->
+<!--                    </div>-->
+<!--                    &lt;!&ndash; Modal body &ndash;&gt;-->
+<!--                    <div class="p-4 md:p-5 space-y-4">-->
+<!--                        <form class="p-5" @submit.prevent="createVendorForm">-->
 
+
+<!--                            <div class="relative z-0 w-full mb-5 group">-->
+<!--                                <input type="text" v-model="props.form.title" id="floating_first_name" placeholder=" " :class="[-->
+<!--                                    'block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer',-->
+<!--                                    props.form.errors.title-->
+<!--                                        ? 'border-red-500 text-red-900 dark:border-red-400 dark:text-red-400 focus:border-red-600'-->
+<!--                                        : 'text-gray-900 border-gray-300 dark:text-white dark:border-gray-600 focus:border-blue-600'-->
+<!--                                ]" />-->
+<!--                                <label for="floating_product_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Product Name</label>-->
+<!--                                <p v-if="props.form.errors.title && props.form.title == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">-->
+<!--                                    {{ props.form.errors.title }}-->
+<!--                                </p>-->
+<!--                            </div>-->
+<!--                            <div class="grid md:grid-cols-2 md:gap-6">-->
+<!--                                <div class="relative z-0 w-full mb-5 group">-->
+<!--                                    <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="props.form.category_id" :class="[-->
+<!--                                    props.form.errors.category_id && props.form.category_id == ''-->
+<!--                                        ? 'border-red-500 text-red-900 dark:border-red-400 dark:text-red-400 focus:border-red-600'-->
+<!--                                        : 'text-gray-900 border-gray-300 dark:text-white dark:border-gray-600 focus:border-blue-600'-->
+<!--                                ]">-->
+
+<!--                                        <option selected disabled>Category</option>-->
+<!--                                        <option v-for="category in props.categories" :key="category.id" :value="category.id">{{ category.title }}</option>-->
+<!--                                    </select>-->
+<!--                                    <p v-if="props.form.errors.category_id && props.form.category_id == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">-->
+<!--                                        {{ props.form.errors.category_id }}-->
+<!--                                    </p>-->
+<!--                                </div>-->
+<!--                                <div class="relative z-0 w-full mb-5 group">-->
+<!--                                    <input type="number" v-model="props.form.qty" id="floating_quantity" :class="[-->
+<!--                                    'block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer',-->
+<!--                                    props.form.errors.qty && props.form.qty == ''-->
+<!--                                        ? 'border-red-500 text-red-900 dark:border-red-400 dark:text-red-400 focus:border-red-600'-->
+<!--                                        : 'text-gray-900 border-gray-300 dark:text-white dark:border-gray-600 focus:border-blue-600'-->
+<!--                                ]" placeholder=" " value="1" />-->
+<!--                                    <label for="floating_quantity" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Quantity</label>-->
+<!--                                    <p v-if="props.form.errors.qty && props.form.qty == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">-->
+<!--                                        {{ props.form.errors.qty }}-->
+<!--                                    </p>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                            <div class="grid md:grid-cols-2 md:gap-6">-->
+<!--                                <div class="relative z-0 w-full mb-5 group">-->
+<!--                                    <input type="number" v-model="props.form.purchase_price" id="floating_phone" :class="[-->
+<!--                                    'block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer',-->
+<!--                                    props.form.errors.purchase_price && props.form.purchase_price == ''-->
+<!--                                        ? 'border-red-500 text-red-900 dark:border-red-400 dark:text-red-400 focus:border-red-600'-->
+<!--                                        : 'text-gray-900 border-gray-300 dark:text-white dark:border-gray-600 focus:border-blue-600'-->
+<!--                                ]" placeholder=" " />-->
+<!--                                    <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Purchase Price ($1499)</label>-->
+<!--                                    <p v-if="props.form.errors.purchase_price && props.form.purchase_price == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">-->
+<!--                                        {{ props.form.errors.purchase_price }}-->
+<!--                                    </p>-->
+<!--                                </div>-->
+<!--                                <div class="relative z-0 w-full mb-5 group">-->
+<!--                                    <input type="number" v-model="props.form.sell_price" id="floating_company" :class="[-->
+<!--                                    'block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer',-->
+<!--                                    props.form.errors.sell_price && props.form.sell_price == ''-->
+<!--                                        ? 'border-red-500 text-red-900 dark:border-red-400 dark:text-red-400 focus:border-red-600'-->
+<!--                                        : 'text-gray-900 border-gray-300 dark:text-white dark:border-gray-600 focus:border-blue-600'-->
+<!--                                ]" placeholder=" " />-->
+<!--                                    <label for="floating_company" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Sell Price ($1999)</label>-->
+<!--                                    <p v-if="props.form.errors.sell_price && props.form.sell_price == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">-->
+<!--                                        {{ props.form.errors.sell_price }}-->
+<!--                                    </p>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                            <div class="grid md:grid-cols-1 md:gap-6">-->
+<!--                                <div class="relative z-0 w-full mb-5 group">-->
+<!--                                    <textarea v-model="props.form.description" id="floating_description" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " ></textarea>-->
+<!--                                    <label for="floating_description" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Description</label>-->
+<!--                                    <p v-if="props.form.errors.description && props.form.description == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">-->
+<!--                                        {{ props.form.errors.description }}-->
+<!--                                    </p>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                            <div >-->
+<!--                                <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Create</button>-->
+<!--                            </div>-->
+<!--                        </form>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <CreateModel :form="form" />-->
 
         <!-- Edit Inventory modal -->
 
--- resources/js/pages/vendors/includes/VendorList.vue
+++ resources/js/pages/vendors/includes/VendorList.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
 
+import {EyeIcon, PenIcon, TrashIcon} from "lucide-vue-next";
+
+const props = defineProps(['vendors']);
 </script>
 
 <template>
-
+    <tr v-for="vendor in props.vendors" :key="vendor.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            {{ vendor.name }}
+        </th>
+        <td class="px-6 py-4">
+            {{ vendor.phone_no }}
+        </td>
+        <td class="px-6 py-4">
+            {{ vendor.address }}
+        </td>
+        <td class="px-6 py-4 flex gap-1">
+            <button class="text-red-600"><TrashIcon/></button>
+            <button class="text-green-600"><PenIcon /></button>
+            <a href="" class="text-blue-600"><EyeIcon /></a>
+        </td>
+    </tr>
 </template>
 
 <style scoped>
