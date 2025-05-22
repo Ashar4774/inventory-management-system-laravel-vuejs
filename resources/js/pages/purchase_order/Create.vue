55d245364dbae4c5f20f1da31347a0cc5934ca42
Ashar4774 <ashar.muhammad74@gmail.com>
2025-05-22 20:34:57 +0500

    set layout for insert inventory in create purchase order

--- resources/js/pages/purchase_order/Create.vue
+++ resources/js/pages/purchase_order/Create.vue
@@ -146,16 +146,55 @@ const form = useForm({
                     <div class="flex mt-2">
                         <div class="w-full rounded-xl border border-sidebar-border/70 dark:border-sidebar-border p-10">
                             <div class="relative z-0 w-full mb-5 group">
-
-                                <input type="text"  id="floating_first_name" placeholder=" " value="0" readonly :class="[
-                                        'block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer'
-                                    ]" />
-                                <label for="floating_product_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Sub Total</label>
-                                <!--                        <p v-if="form.errors.title && form.title == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">
-                                                            {{ form.errors.title }}
-                                                        </p>-->
+                                <a href="" class="text-blue-600 underline">Add new inventory</a>
                             </div>
-                            <div class="grid md:grid-cols-2 md:gap-6">
+                            <div class="grid md:grid-cols-6 md:gap-6" id="">
+                                <div class="relative z-0 w-full mb-5 group">
+                                    <input type="text"  id="floating_product_name" list="productOptions" placeholder=" " value="" :class="[
+                                        'block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer text-capitalize'
+                                    ]" />
+                                    <label for="floating_product_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Product Name</label>
+                                    <datalist id="productOptions" class="hidden border-b border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+
+<!--                                        <option selected disabled>Category</option>-->
+                                        <option value="amount" />
+                                        <option value="percent" />
+                                        <!--                                <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.title }}</option>-->
+                                    </datalist>
+                                    <!--                        <p v-if="form.errors.title && form.title == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">
+                                                                {{ form.errors.title }}
+                                                            </p>-->
+                                </div>
+                                <div class="relative z-0 w-full mb-5 group">
+                                    <select id="countries" class=" border-b border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+
+                                        <option selected disabled>Category</option>
+                                        <option value="amount">Amount</option>
+                                        <option value="percent">Percent</option>
+                                        <!--                                <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.title }}</option>-->
+                                    </select>
+                                    <!--                            <p v-if="form.errors.category_id && form.category_id == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">
+                                                                    {{ form.errors.category_id }}
+                                                                </p>-->
+                                </div>
+                                <div class="relative z-0 w-full mb-5 group">
+                                    <input type="number"  id="floating_quantity" :class="[
+                                        'block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer'
+                                    ]" placeholder=" " value="" />
+                                    <label for="floating_quantity" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Quantity</label>
+                                    <!--                            <p v-if="form.errors.qty && form.qty == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">
+                                                                    {{ form.errors.qty }}
+                                                                </p>-->
+                                </div>
+                                <div class="relative z-0 w-full mb-5 group">
+                                    <input type="number"  id="floating_quantity" :class="[
+                                        'block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer'
+                                    ]" placeholder=" " value="" />
+                                    <label for="floating_quantity" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Purchase Price</label>
+                                    <!--                            <p v-if="form.errors.qty && form.qty == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">
+                                                                    {{ form.errors.qty }}
+                                                                </p>-->
+                                </div>
                                 <div class="relative z-0 w-full mb-5 group">
                                     <input type="number"  id="floating_quantity" :class="[
                                         'block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer'
@@ -165,8 +204,8 @@ const form = useForm({
                                                                     {{ form.errors.qty }}
                                                                 </p>-->
                                 </div>
-                                <div class="relative z-0 w-full mb-5 group">
-                                    <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                                <div class="relative z-0 w-full mb-5 group flex gap-3">
+                                    <select id="countries" class=" border-b border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
 
                                         <option selected disabled>Discount type</option>
                                         <option value="amount">Amount</option>
@@ -176,21 +215,12 @@ const form = useForm({
                                     <!--                            <p v-if="form.errors.category_id && form.category_id == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">
                                                                     {{ form.errors.category_id }}
                                                                 </p>-->
+                                    <button class="text-red-600"><TrashIcon/></button>
                                 </div>
                             </div>
-                            <div class="relative z-0 w-full mb-5 group">
-
-                                <input type="text"  id="floating_first_name" placeholder=" " value="0" readonly :class="[
-                                        'block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer'
-                                    ]" />
-                                <label for="floating_product_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Total Amount</label>
-                                <!--                        <p v-if="form.errors.title && form.title == ''" class="mt-1 text-sm text-red-600 dark:text-red-500">
-                                                            {{ form.errors.title }}
-                                                        </p>-->
+                            <div class="text-right">
+                                <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Create</button>
                             </div>
-                                                    <div >
-                                                        <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Create</button>
-                                                    </div>
                         </div>
                     </div>
                 </form>
